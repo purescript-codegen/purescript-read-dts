3b28649e820fa0cc85cbfb78e7fe3e07f75a3ac1
Tomasz Rybarczyk <paluho@gmail.com>
2019-09-20 08:21:52 +0200

    Cosmetics

--- src/ReadDTS/Instantiation.purs
+++ src/ReadDTS/Instantiation.purs
@@ -5,14 +5,23 @@ import Data.Map (Map)
 -- | Not our current problems:
 -- |
 -- | * How to represent recursive cycles so they can be useful?
+-- |
 -- | * Should we provide some lazy evaluating value in place of recursive type?
+-- |
 -- | * Should we provide `fullyQualifiedNames` somewhere too?
 
 -- | Current problems:
+-- |
 -- | * We want to parametrize this by itself so we can use recursion schemes on this type.
+-- |
 -- | * We want to move `AST.apply*` here and rename it probably `Instatiation.typeNode`
 -- | `Instantiation.application`.
--- | * We want to start from the basic types up :-)
+-- |
+-- | * We want to start from the basic types up so let's start to materia-ui Badge.d.ts and provide
+-- | some kind of "tests" for it.
+-- |
+-- | * Do we really want to expand ts intersections here? Do we lose something? How we are going
+-- | to treat intersections which contain 'Uknown'?
 
 data Type
   = Array Type
