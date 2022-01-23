73fc7d89ba98aae67469bb7985527d00a6d61719
Tomasz Rybarczyk <paluho@gmail.com>
2022-01-23 10:11:39 +0100

    Add test for extend interface type flattening

--- test/RecType.purs
+++ test/RecType.purs
@@ -124,16 +124,28 @@ suite = Test.suite "Recursive type repr" do
         , type: roll $ AST.TsTypeRef $ FullyQualifiedName "\"Root\".Y"
         }
 
-  testXShouldEqual "export type Y = { m: number }; export interface X{ m: { n: Y }}" $
+  testXShouldEqual "export type Y = { yp: string }; export interface X{ xp: { xpp: Y }}" $
     roll $ AST.TsInterface $ Array.singleton
-      { name: "m"
+      { name: "xp"
       , optional: false
       , type: roll $ AST.TsObject $ Array.singleton
-          { name: "n"
+          { name: "xpp"
           , optional: false
           , type: roll $ AST.TsTypeRef $ FullyQualifiedName "\"Root\".Y"
           }
       }
+
+  testXShouldEqual "export type Y = { m: number }; export interface X extends Y { n: string }" $
+    roll $ AST.TsInterface
+      [ { name: "n"
+        , optional: false
+        , type: roll $ AST.TsString
+        }
+      , { name: "m"
+        , optional: false
+        , "type": roll AST.TsNumber
+        }
+      ]
 
   do
     let
