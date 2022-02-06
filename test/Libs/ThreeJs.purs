a6158101aabe5c4660fc9f1a0b14b791bc470ef7
Tomasz Rybarczyk <paluho@gmail.com>
2022-02-06 23:55:49 +0100

    Add three.js modules for testing

---
+++ test/Libs/ThreeJs.purs
@@ -0,0 +1,12 @@
+module Test.Libs.ThreeJs where
+
+-- | Testing version:
+-- |    "@types/three": "^0.137.0"
+-- |
+-- | Currenly testing a subset of three.js modules.
+-- |
+-- | These files were drastically simplified to cut out
+-- | inclusion of additional ts modules:
+-- |
+-- | * `core/Object3D.d.ts`
+-- | * `objects/Sprite.d.ts`
